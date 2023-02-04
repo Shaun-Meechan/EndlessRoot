def63fa9622299362b0221e8df6a9aa968110286
Lun4tix <rodrigoramos1238@gmail.com>
2023-02-04 22:20:08 +0000

    health bar that decreases over time

--- Into The Soil/Assets/Scripts/Health.cs
+++ Into The Soil/Assets/Scripts/Health.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    private float currentHealth = 100f;
+    private float maxHealth = 100f;
+    private float currentHealth;
+    private float healthDecreasedPerSecond = 2f;
+
+    [SerializeField] private Image healthBar;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
 
     public float GetHealth()
     {
@@ -15,5 +25,19 @@ public class Health : MonoBehaviour
     {
         currentHealth += change;
 
+    }
+
+    /*
+    public void SetBarSize(float sizeNormalized)
+    {
+        healthBar.transform.localScale = new Vector3(sizeNormalized, 1f);
+    }
+    */
+
+    private void Update()
+    {
+        currentHealth -= healthDecreasedPerSecond * Time.deltaTime;
+        //SetBarSize(currentHealth/100);
+        healthBar.fillAmount = currentHealth / maxHealth;
     }
 }
